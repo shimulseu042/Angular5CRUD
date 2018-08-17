24bf95f553acad784982dfdea457753107685b58
shimulseu042 <shimul.cse012@gmail.com>
2018-08-17 20:04:43 +0600

    after dropdown finished

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import {Department} from '../models/department.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,12 +8,27 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-  
+  departments: Department[] = [
+    {
+      id: 1, name:'Help Desk'
+    },
+    {
+      id: 2, name:'HR'
+    },
+    {
+      id: 3, name:'Payroll'
+    },
+    {
+      id: 4, name:'IT'
+    }
+];
+  // isActive = true;
+  // department = "3";
   constructor() { }
 
   ngOnInit() {
   }
   saveEmployee(empForm: NgForm): void{
-    console.log(empForm);
+    console.log(empForm.value);
   }
 }
